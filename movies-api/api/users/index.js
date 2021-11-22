e73869a258645dbe7c03e30432a073c552dde9c6
Paul Shanahan <pshaners10@gmail.com>
2021-11-22 12:31:40 +0000

    Mongo lab User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,6 +9,29 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
+    }
+});
+
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
   // register(Create)/Authenticate User
   router.post('/', async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
@@ -18,6 +41,8 @@ router.get('/', async (req, res) => {
             msg: 'Successful created new user.',
         });
     }
+
+    
     else {  //Must be an authenticate then!!! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
         if (!user) {
