bf5aae55f661d4df68970c01271c560fb6bb2564
Paul Shanahan <pshaners10@gmail.com>
2021-11-22 12:40:27 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -22,6 +23,25 @@ router.post('/:id/favourites', async (req, res) => {
         }
     }
 });
+
+  // register
+  router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await User(req.body).save()
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
+    }
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+}));
 
 router.get('/:id/favourites', async (req, res) => {
     const user = await User.findById(req.params.id);
