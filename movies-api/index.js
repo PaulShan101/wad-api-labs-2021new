fecb5955c97ac4ab2718fc5702ae18d54bad19e4
Paul Shanahan <pshaners10@gmail.com>
2021-11-22 11:55:29 +0000

    Mongo lab Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,16 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
+import './db';
+import './seedData'
 
 dotenv.config();
 
 const app = express();
+
+
 
 app.use(express.json());
 
 const port = process.env.PORT;
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
